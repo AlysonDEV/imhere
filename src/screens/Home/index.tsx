0d7e55e8afb9edbbafa1cac81a82e364e5d96256
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-01-25 20:51:50 -0300

    button add partipant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { styles } from "./styles";
 
 export function Home () {
+
+  function handlePartipantAdd() {
+    console.log("Você clicou no botão de adcionar")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
@@ -13,6 +18,11 @@ export function Home () {
         placeholderTextColor="#6B6B6B" 
         keyboardAppearance="dark"
       />
+      <TouchableOpacity style={styles.button} onPress={handlePartipantAdd}>
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
       <StatusBar style='light' />
     </View>
   )
