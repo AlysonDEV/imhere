147d7e5327e22e161c0d721d2a53120f9276bb30
Alyson Ronnan Martins <96950870+AlysonDEV@users.noreply.github.com>
2024-01-25 21:39:27 -0300

    Creating the form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,25 +4,30 @@ import { styles } from "./styles";
 
 export function Home () {
 
+
   function handlePartipantAdd() {
     console.log("Você clicou no botão de adcionar")
   }
+
+
 
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022.</Text>
-      <TextInput 
-        style={styles.input} 
-        placeholder="Nome do participante" 
-        placeholderTextColor="#6B6B6B" 
-        keyboardAppearance="dark"
-      />
-      <TouchableOpacity style={styles.button} onPress={handlePartipantAdd}>
-        <Text style={styles.buttonText}>
-          +
-        </Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Nome do participante" 
+          placeholderTextColor="#6B6B6B" 
+          keyboardAppearance="dark"
+        />
+        <TouchableOpacity style={styles.button} onPress={handlePartipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style='light' />
     </View>
   )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -22,7 +22,8 @@ export const styles = StyleSheet.create({
     borderRadius: 5,
     color: "#FFF",
     padding: 16,
-    fontSize: 16
+    fontSize: 16,
+    flex: 1
   },
   button: {
     width: 56,
@@ -35,5 +36,12 @@ export const styles = StyleSheet.create({
   buttonText: {
     color: "#FFF",
     fontSize: 24
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 12,
+    marginTop: 36,
+    marginBottom: 46
   }
 });
